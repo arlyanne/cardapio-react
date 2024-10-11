b127a8b994e1ea1629403208154efa6591cdd414
Arlyanne <arlyanne.lima.ferro@gmail.com>
2024-10-11 09:05:26 -0300

    Criação de rotas

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import './index.css'
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./assets/components/Header/index.tsx";
+import RouteApp from "./routes/index.tsx";
 
 
 createRoot(document.getElementById("root")!).render(
@@ -12,7 +12,7 @@ createRoot(document.getElementById("root")!).render(
     <ChakraProvider>
       <StrictMode>
         <Header/>
-        <App />
+        <RouteApp/>
       </StrictMode>,
     </ChakraProvider>
   </BrowserRouter>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,13 @@
+import { Route, Routes } from "react-router-dom"
+import Cashier from "../pages/cashier"
+
 const RouteApp = () => {
+    return(
+        <Routes>
+            <Route element={<Cashier/>} path="/cashier" ></Route>
+        </Routes>
+    )
     
 }
+
+export default RouteApp;
